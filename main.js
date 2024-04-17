325c224a7b21721f4a2ed1f5af12f586020e81de
Ghulam-Ahmed-1357 <155989475+Ghulam-Ahmed-1357@users.noreply.github.com>
2024-04-17 18:54:36 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "Sentence",
+        message: "Enter your sentence you want to count it's words"
+    }
+]);
+const words = answer.Sentence.trim().split(" "); // word is of array type , 
+// trim() method remove whitespaces from beginning and end of the sentence (but not remove spaces in between words)
+// split() method split the sentence into words as i provided the (" ") space in double quotes, if i not provide the space it will split sentence into characters
+console.log(words);
+console.log(`Your sentence contains ${words.length} words`);
